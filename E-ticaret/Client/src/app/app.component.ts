2b9aadf63cbac6ef7d7e296f15a76a30bab6a08f
AlparslanTR <bybluestht@gmail.com>
2024-09-04 23:52:39 +0300

    -- V.1.17 -- - Admin Template Design Edilip Gömüldü. - Temanın 16 Dil İçin Çeviriler Eklendi. - Docker Tarafında Yeni Düzenleme ve Eklemeler Yapıldı.

--- E-ticaret/Client/src/app/app.component.ts
+++ E-ticaret/Client/src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { filter, map, switchMap, tap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'Client';
+    constructor(
+        private router: Router,
+        private activatedRoute: ActivatedRoute,
+        private titleService: Title,
+        private translate : TranslateService
+    ) {
+        this.router.events
+            .pipe(
+                filter((event) => event instanceof NavigationEnd),
+                map(() => this.activatedRoute),
+                map((route) => {
+                    while (route.firstChild) route = route.firstChild;
+                    return route;
+                }),
+                filter((route) => route.outlet === 'primary'),
+                switchMap((route) => {
+                    return route.data.pipe(
+                        map((routeData: any) => {
+                            const title = routeData['title'];
+                            return { title };
+                        }),
+                    );
+                }),
+                tap((data: any) => {
+                    let title = data.title;
+                    
+                    this.translate.get('title_content').subscribe((translatedTitle: string) => {
+                        title = (title ? title + ' | ' : '') + translatedTitle;
+                        this.titleService.setTitle(title);
+                    });
+                })
+            )
+            .subscribe();
+    }
 }
